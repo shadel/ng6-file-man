8e564d0eba76497d749b6ff1dba1bb878b41fa90
trieu.vo <trieugia008@gmail.com>
2019-06-21 16:48:45 +0700

    update module

--- file-manager/file-manager.module.d.ts
+++ file-manager/file-manager.module.d.ts
@@ -1,3 +1,4 @@
+import { ModuleWithProviders } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActionReducerMap } from '@ngrx/store';
 import { AppStore } from './reducers/reducer.factory';
@@ -5,4 +6,5 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export declare function createTranslateLoader(http: HttpClient): TranslateHttpLoader;
 export declare function getReducers(): ActionReducerMap<AppStore>;
 export declare class FileManagerModule {
+    static forRoot(): ModuleWithProviders;
 }
